3298b94147b0bb0f3264676f197b953ff09d9c91
Danielborbafs <dev.danielborba@gmail.com>
2023-03-26 19:37:44 -0300

    Components

---
+++ fundamentos/src/pages/exemplo.jsx
@@ -0,0 +1,12 @@
+
+import Cabecalho from "../components/Cabecalho"
+
+export default function Exemplo() {
+    return (
+        <div>
+              < Cabecalho titulo="Next.js & React" />
+              < Cabecalho titulo="Aprende next na prática" />
+        </div>
+    
+    )
+}
--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -1,10 +1,12 @@
+import Link from 'next/link'
+
+
 export default function Inicio() {
     return (
         <div>
-            <h1>
-                Fundamentos de Next.js E React
-            </h1>
-            <h2>Vamos estudar esse framework</h2>      
+           <Link href="/estiloso">
+                estiloso
+           </Link>
         </div>
     )
 }
